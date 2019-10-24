56297108971c4e2f705888fd7b1fa86e5aa1a0ca
ShiniYanada <ysthon@gmail.com>
2019-10-24 10:32:39 +0900

    Initial Commit

---
+++ CUMA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CUMA
+//
+//  Created by 簗田信緯 on 2019/10/24.
+//  Copyright © 2019 Shini Yanada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
