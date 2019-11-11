9521af5f57ccf16d8e510351ff541a0b957048af
ShiniYanada <ysthon@gmail.com>
2019-11-11 13:33:05 +0900

    時間割の設定と変更のviewControllerを追加

---
+++ CUMA/TimeTableEditViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimeTableEditViewController.swift
+//  CUMA
+//
+//  Created by 簗田信緯 on 2019/11/11.
+//  Copyright © 2019 Shini Yanada. All rights reserved.
+//
+
+import UIKit
+
+class TimeTableEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CUMA/TimeTableListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimeTableListViewController.swift
+//  CUMA
+//
+//  Created by 簗田信緯 on 2019/11/11.
+//  Copyright © 2019 Shini Yanada. All rights reserved.
+//
+
+import UIKit
+
+class TimeTableListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
