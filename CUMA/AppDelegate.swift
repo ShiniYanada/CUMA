4fbb03cab542767cd14b2e23ce677912fc0eb5ad
ShiniYanada <ysthon@gmail.com>
2019-12-22 14:36:46 +0900

    時間割の初期データにselectedプロパティを追加し、schemaVersionを2に変更

--- CUMA/AppDelegate.swift
+++ CUMA/AppDelegate.swift
@@ -16,17 +16,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let defaultURL = Realm.Configuration.defaultConfiguration.fileURL!
+        let config = Realm.Configuration(
+            schemaVersion: 2,
+            migrationBlock: { migration, oldSchemaVersion in
+                if (oldSchemaVersion < 2) {
+
+                }
+            }
+        )
+        Realm.Configuration.defaultConfiguration = config
+        let realm = try! Realm()
         
-        if FileManager.default.fileExists(atPath: defaultURL.path) {
-            print("exist")
-        } else {
-            print("not exist")
-            let bundleRealmPath = Bundle.main.url(forResource: "default-compact", withExtension: "realm")
-            do {
-                try FileManager.default.copyItem(at: bundleRealmPath!, to: defaultURL)
-            } catch { print("coppy error ")}
-        }
+        let result = realm.objects(TimeTable.self)
+        print(result.first?.selected == true)
         
         return true
     }
@@ -45,6 +47,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func createInitialData(){
+        // 初期データ
+        let timeTable = TimeTable()
+        timeTable.name = "時間割1"
+        timeTable.selected = true
+        // schemaVersionの設定
+        let config = Realm.Configuration(
+            schemaVersion: 2,
+            migrationBlock: { migration, oldSchemaVersion in
+                if (oldSchemaVersion < 2) {
+
+                }
+            }
+        )
+        Realm.Configuration.defaultConfiguration = config
+        let realm = try! Realm()
+        //初期データを追加
+        realm.deleteAll()
+        try! realm.write {
+            realm.add(timeTable)
+        }
+        
+        let defaultURL = Realm.Configuration.defaultConfiguration.fileURL!
+        let defaultParentURL = defaultURL.deletingLastPathComponent()
+        //realm圧縮データのファイルパスを設定
+        let compactedURL = defaultParentURL.appendingPathComponent("initial-compact.realm")
+        print(defaultURL)
+        // 圧縮データファイルパスに圧縮データを作成
+        try! realm.writeCopy(toFile: compactedURL)
+        // 既存の初期データを削除
+        try! FileManager.default.removeItem(at: defaultURL)
+        
+        // default.realmにデータが存在していれば何もしない
+        // 存在していなかったら圧縮初期データをdefault.realmに追加
+        if FileManager.default.fileExists(atPath: defaultURL.path) {
+            print("exist")
+        } else {
+            print("not exist")
+            let bundleRealmPath = Bundle.main.url(forResource: "initial-compact", withExtension: "realm")
+            do {
+                try FileManager.default.copyItem(at: bundleRealmPath!, to: defaultURL)
+            } catch { print("coppy error")}
+        }
+    }
 
 }
 
